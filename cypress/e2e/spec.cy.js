6f8bf211349cdf832b7287192e5e6e116959f102
Abigail Barengo <abiibar2002@gmail.com>
2024-07-16 22:30:39 -0300

    primer testing e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:8080/')
   })
-})
+}) // ejemplo predeterminado de testeo de cypress
+
+
+// primer testeo: usuario valido para el incio de sesion
+describe('Primer testeo', () => {
+  it('Ingresar con usuario válido', () => {
+    cy.visit('http://localhost:8080/');   // visitar la pagina principal
+    cy.get('#Usuario').type('usuario_valido');   // entrar con usuario valido
+  });
+});
+
+
+// realizar mas testeos y en otras partes de la pagina tambien.
